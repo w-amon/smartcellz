81ea3d75b62e80fa092c45992b54bafff9691f87
w-amon <xxx110680@gmail.com>
2023-11-07 09:11:47 +0300

    Modified app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidenavComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
